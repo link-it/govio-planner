fd092c74a5babe320be89c832068948459f27623
fscarlato <f.scarlato@protonmail.com>
2023-04-26 13:09:20 +0200

    Rimossa dipendenza da govio. Aggiunto filtro byName

--- api/src/main/java/it/govhub/govio/planner/api/filters/ExpirationFileFilters.java
+++ api/src/main/java/it/govhub/govio/planner/api/filters/ExpirationFileFilters.java
@@ -7,6 +7,7 @@ import javax.persistence.criteria.Root;
 import org.springframework.data.jpa.domain.Specification;
 
 import it.govhub.govio.planner.api.entity.ExpirationFileEntity;
+import it.govhub.govio.planner.api.entity.ExpirationFileEntity_;
 
 public class ExpirationFileFilters {
 
@@ -16,9 +17,11 @@ public class ExpirationFileFilters {
 			return (Root<ExpirationFileEntity> root, CriteriaQuery<?> query, CriteriaBuilder cb) -> null; 
 		}
 
-		public static Specification<ExpirationFileEntity> byName(String string) {
-			// TODO Auto-generated method stub
-			return null;
+		public static Specification<ExpirationFileEntity> byName(String name) {
+			return (Root<ExpirationFileEntity> root, CriteriaQuery<?> query, CriteriaBuilder cb) -> 
+				cb.like(
+						cb.lower(root.get(ExpirationFileEntity_.name)),
+						"%"+name.toLowerCase()+"%");
 		}
 
 		public static Specification<ExpirationFileEntity> byGovioFilename(String string) {
