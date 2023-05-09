d1cf00cf4ac1d75989035e221280d8b2bc85aae4
Marco Flagella <marco.flagella@gmail.com>
2023-05-09 19:38:57 +0200

    Fix su expedition date, e commenti nel file di properties

--- batch/src/main/java/it/govhub/govio/planner/batch/step/LookForLastDateTasklet.java
+++ batch/src/main/java/it/govhub/govio/planner/batch/step/LookForLastDateTasklet.java
@@ -22,6 +22,7 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.OffsetDateTime;
+import java.time.ZoneId;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -41,35 +42,36 @@ import it.govhub.govio.planner.batch.repository.GovioFileProducedRepository;
  * Se non c'è una iterazione precedente del batch, viene presa la data dal file di properties.
  * 
  */
-	public class LookForLastDateTasklet implements Tasklet {
-		@Value("${planner.ntfy.min-last-date}")
-		private String lastDate;
-		@Value("${planner.ntfy.date-time}")
-		private String dateTime;
-		@Value("${planner.ntfy.delay-days}")
-		private long daysDelay;
-		@Value("${planner.ntfy.delay-time}")
-		private long hoursDelay;
+public class LookForLastDateTasklet implements Tasklet {
+	@Value("${planner.ntfy.min-last-date}")
+	private String lastDate;
+	@Value("${planner.ntfy.date-time}")
+	private String dateTimeDelay;
 
-		@Autowired
-		GovioFileProducedRepository govioFileProducedRepository;
-	
-		private Logger logger = LoggerFactory.getLogger(LookForLastDateTasklet.class);
+	@Autowired
+	GovioFileProducedRepository govioFileProducedRepository;
+
+	private Logger logger = LoggerFactory.getLogger(LookForLastDateTasklet.class);
 
 
-		@Override
-		public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) {
-			OffsetDateTime date = govioFileProducedRepository.lastDateNotifyFile();
-			ExecutionContext jobExecutionContext = chunkContext.getStepContext().getStepExecution().getJobExecution().getExecutionContext();
-			jobExecutionContext.put("date",(date==null)? lastDate: date.toString());
-			logger.info("Data in cui è girato il batch l'ultima volta: {}",(date==null)? lastDate: date.toString());
+	@Override
+	public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) {
+		OffsetDateTime date = govioFileProducedRepository.lastDateNotifyFile();
+		ExecutionContext jobExecutionContext = chunkContext.getStepContext().getStepExecution().getJobExecution().getExecutionContext();
+		jobExecutionContext.put("date",(date==null)? lastDate: date.toString());
+		logger.info("Data in cui è girato il batch l'ultima volta: {}",(date==null)? lastDate: date.toString());
+		LocalDateTime expeditionDate;
+		// se il parametro è null
+		// se sono passate le 12:00 metto data di spedizione a domani a mezzogiorno, sennò oggi a mezzogiorno
+		LocalTime noon = LocalTime.NOON;
+		LocalDate today = LocalDate.now(ZoneId.of("Europe/Berlin"));
+		expeditionDate = LocalDateTime.of(today, noon);
+		if (LocalTime.now().getHour()>12) expeditionDate = expeditionDate.plusDays(1);
+		// in caso il parametro dateTimeDelay non sia vuoto, viene settato un ritardo alla spedizione del messaggio
+		if (dateTimeDelay!=null) expeditionDate.plusHours(Long.valueOf(dateTimeDelay));
+		jobExecutionContext.put("expeditionDate",expeditionDate.toString());
+		logger.info("Expedition date: {}",expeditionDate);
 
-			LocalTime expeditionHour = ((dateTime==null) ? LocalTime.NOON : LocalTime.parse(dateTime));
-			// calcola la data e l'orario di spedizione, sommando ai valori di default e i parametri di configurazione
-			LocalDateTime expeditionDate = LocalDateTime.of(LocalDate.now().plusDays(daysDelay), expeditionHour.plusHours(hoursDelay));
-			jobExecutionContext.put("expeditionDate",expeditionDate.toString());
-			logger.info("Expedition date: {}",expeditionDate);
-
-			return null;
-		}
+		return null;
+	}
 }
