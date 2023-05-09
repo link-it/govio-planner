d0f56c9a164eda8b97cdd84f83bb93b6a280fd56
fscarlato <f.scarlato@protonmail.com>
2023-05-09 15:33:21 +0200

    Fixati copyright notices e application.properties

--- api/src/main/java/it/govhub/govio/planner/api/web/FileController.java
+++ api/src/main/java/it/govhub/govio/planner/api/web/FileController.java
@@ -1,8 +1,27 @@
+/*******************************************************************************
+ *  GovIO Planner - Notification system Planner for AppIO
+ *  
+ *  Copyright (c) 2021-2023 Link.it srl (http://www.link.it).
+ *  
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License version 3, as published by
+ *  the Free Software Foundation.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *  
+ *******************************************************************************/
 package it.govhub.govio.planner.api.web;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.nio.file.Path;
+import java.util.HashSet;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
@@ -15,7 +34,6 @@ import org.apache.tomcat.util.http.fileupload.servlet.ServletFileUpload;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
 import org.springframework.data.domain.Page;
@@ -56,7 +74,6 @@ import it.govhub.govregistry.commons.utils.LimitOffsetPageRequest;
 import it.govhub.govregistry.commons.utils.ListaUtils;
 import it.govhub.govregistry.commons.utils.RequestUtils;
 import it.govhub.security.services.SecurityService;
-import java.util.HashSet;
 
 
 @V1RestController
