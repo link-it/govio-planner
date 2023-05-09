0789a788e51fae0a0c69532fbb10d1ea61f69210
fscarlato <f.scarlato@protonmail.com>
2023-05-09 15:33:21 +0200

    Fixati copyright notices e application.properties

--- api/src/main/java/it/govhub/govio/planner/api/services/PlannerFileService.java
+++ api/src/main/java/it/govhub/govio/planner/api/services/PlannerFileService.java
@@ -1,3 +1,21 @@
+/*******************************************************************************
+ *  GovIO Planner - Notification system Planner for AppIO
+ *  
+ *  Copyright (c) 2021-2023 Link.it srl (http://www.link.it).
+ *  
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License version 3, as published by
+ *  the Free Software Foundation.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *  
+ *******************************************************************************/
 package it.govhub.govio.planner.api.services;
 
 import java.io.File;
@@ -78,31 +96,4 @@ public class PlannerFileService {
     	return this.fileRepo.save(file);
 	}
 	
-    	
-
-/*	@Transactional
-	public FileMessageList listFileMessages(Specification<GovioFileMessageEntity> spec, LimitOffsetPageRequest pageRequest) {
-		
-		// TODO: Qui ho bisogno di un'entity graph che di ogni fileEntity mi peschi anche i
-		// fileMessages, altrimenti pago altre
-		// N query quando vado a convertire i files
-		
-		return null;
-		
-		Page<GovioFileMessageEntity> fileList = this.fileMessageRepo.findAll(spec, pageRequest.pageable);
-
-		HttpServletRequest curRequest = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes())
-				.getRequest();
-
-		FileMessageList ret = ListaUtils.buildPaginatedList(fileList, pageRequest.limit, curRequest,
-				new FileMessageList());
-
-		for (GovioFileMessageEntity fileMessage : fileList) {
-			ret.addItemsItem(this.fileMessageAssembler.toModel(fileMessage));
-		}
-		
-		return ret;
-	}
-	
-	*/
 }
