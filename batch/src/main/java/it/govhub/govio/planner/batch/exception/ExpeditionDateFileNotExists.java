b0dc3677649c62ddab65adb85f2a6e03ea898737
Marco Flagella <marco.flagella@gmail.com>
2023-05-23 17:58:07 +0200

    Aggiunti funzioni di utility dei test e eccezione personalizzata nel caso manchi il file delle scadenze

---
+++ batch/src/main/java/it/govhub/govio/planner/batch/exception/ExpeditionDateFileNotExists.java
@@ -0,0 +1,14 @@
+package it.govhub.govio.planner.batch.exception;
+
+import java.io.IOException;
+
+
+public class ExpeditionDateFileNotExists extends IOException {
+
+		private static final long serialVersionUID = 1L;
+
+		public ExpeditionDateFileNotExists(String string) {
+			super(string);
+	}
+
+}
